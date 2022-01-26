270091bfc754592dc6404ac11a99e328a6f78d87
Meryem <89212084+meriemsermoune@users.noreply.github.com>
2022-01-26 10:01:15 -0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[]args){
+        // to ignore files you dont wanna push to Git
+        // copy the file and copy to project  name
+        //click on .idea
+        System.out.println("First");
+        System.out.println("Second");
+    }
+}
